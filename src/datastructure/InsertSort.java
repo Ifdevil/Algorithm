c51887eb4afeaa672e9cec3352cdc0722eb5b15b
ifdevil <384867771@qq.com>
2019-08-11 13:00:57 +0800

    直接插入排序、折半插入排序、希尔排序

--- src/datastructure/InsertSort.java
+++ src/datastructure/InsertSort.java
@@ -8,7 +8,7 @@ public class InsertSort {
 
     public static void main(String[] args) {
         int[] a = {2,1,3100,456,21,0,5,6,8};
-        insertSort2(a,a.length);
+        insertSort3(a,a.length);
         for (int i = 0; i < a.length; i++) {
             System.out.println(a[i]);
         }
@@ -65,6 +65,28 @@ public class InsertSort {
                     n[j+1] = n[j];
                 }
                 n[high+1] = temp;
+            }
+        }
+    }
+
+    /**
+     * 希尔排序
+     * @param n
+     * @param len
+     */
+    public static void insertSort3(int[] n,int len){
+        int i;
+        int j;
+        int temp;
+        for(int d = len/2;d>=1;d /= 2){
+            for (i = d;i<len;i++){
+                temp = n[i];
+                if(n[i-d]>n[i]){
+                    for (j=i-d;j>=0&&n[j]>temp;j-=d){
+                        n[j+d] = n[j];
+                    }
+                    n[j+d] = temp;
+                }
             }
         }
     }
