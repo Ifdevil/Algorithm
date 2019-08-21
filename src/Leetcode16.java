6fb4759d58f7fdef9d3048fff2b3bc1a181b9f20
ifdevil <384867771@qq.com>
2019-08-21 18:03:23 +0800

    最接近的三数之和

---
+++ src/Leetcode16.java
@@ -0,0 +1,34 @@
+import java.util.Arrays;
+
+public class Leetcode16 {
+
+
+    public static void main(String[] args) {
+        int[] nums = {0,2,1,-3};
+        System.out.println(threeSumClosest(nums,1));
+    }
+
+    public static int threeSumClosest(int[] nums, int target) {
+        // 排序
+        Arrays.sort(nums);
+        int min = Integer.MAX_VALUE;
+        for (int k = 0; k < nums.length - 2; k++) {
+            int i = k+1;
+            int j = nums.length-1;
+            while (i<j){
+                int sum = nums[k] + nums[i] + nums[j];
+                if(Math.abs(target - sum) < Math.abs(target - min))
+                    min = sum;
+                if(sum > target)
+                    j--;
+                else if(sum < target)
+                    i++;
+                else
+                    return min;
+
+            }
+        }
+        return min;
+    }
+
+}
