73b2f67bbb6c8c8f17f65c0ed543407d5fb00a91
ifdevil <384867771@qq.com>
2019-09-18 10:07:32 +0800

    合并两个有序链表

---
+++ src/Leetcode21.java
@@ -0,0 +1,66 @@
+import java.util.List;
+
+/**
+ * 合并两个有序链表
+ */
+public class Leetcode21 {
+
+    public static void main(String[] args) {
+
+        ListNode l1 = null;
+
+        ListNode l2 = new ListNode(0);
+
+        ListNode merg = mergeTwoLists(l1,l2);
+        while (merg!=null){
+            System.out.println(merg.val);
+            merg = merg.next;
+        }
+    }
+
+    public static ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null) {
+            return l2;
+        }
+        else if (l2 == null) {
+            return l1;
+        }else if (l1.val < l2.val) {
+            l1.next = mergeTwoLists(l1.next, l2);
+            return l1;
+        }
+        else {
+            l2.next = mergeTwoLists(l1, l2.next);
+            return l2;
+        }
+    }
+
+    public static ListNode mergeTwoLists2(ListNode l1, ListNode l2){
+        ListNode prehead = new ListNode(-1);
+
+        ListNode prev = prehead;
+
+        while (l1 != null && l2 != null) {
+            if (l1.val <= l2.val) {
+                prev.next = l1;
+                l1 = l1.next;
+            } else {
+                prev.next = l2;
+                l2 = l2.next;
+            }
+            prev = prev.next;
+        }
+
+        // exactly one of l1 and l2 can be non-null at this point, so connect
+        // the non-null list to the end of the merged list.
+        prev.next = l1 == null ? l2 : l1;
+
+        return prehead.next;
+    }
+
+
+    public static class ListNode {
+      int val;
+      ListNode next;
+      ListNode(int x) { val = x; }
+    }
+}
